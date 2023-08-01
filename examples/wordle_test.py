877dcf8ff6e204a8995c66de22ec73b8b9750b01
Michael Mintz <mdmintz@gmail.com>
2023-08-01 12:52:28 -0400

    Update the Wordle test

--- examples/wordle_test.py
+++ examples/wordle_test.py
@@ -59,6 +59,7 @@ class WordleTests(BaseCase):
     def test_wordle(self):
         self.skip_if_incorrect_env()
         self.open("https://www.nytimes.com/games/wordle/index.html")
+        self.click_if_visible("button.purr-blocker-card__button", timeout=2)
         self.click_if_visible('button:contains("Play")', timeout=2)
         self.click_if_visible('svg[data-testid="icon-close"]', timeout=2)
         self.remove_elements("div.place-ad")
